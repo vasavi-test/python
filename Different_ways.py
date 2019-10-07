2435588a0dd5d4cf39897842af08fc79b1e9e675
Ramakrishna Kalyanapu <ramakrishnakalyanapu@Ramakrishnas-MacBook-Air.local>
2019-10-07 18:57:23 +0530

    different ways script

---
+++ Different_ways.py
@@ -0,0 +1,9 @@
+course="vasavikrishna"
+print len(course)
+print course.lower()
+print course.upper()
+print course.replace("vasavi","krishna")
+print course.find('k')
+print course.count('a')
+print course.islower()
+print course.isupper()
